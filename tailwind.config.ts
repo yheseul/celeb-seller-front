624d65127a31219a6e7ac3c9d7d3c97d28e625bc
yheseul <kangyheseul@naver.com>
2025-03-03 23:14:31 +0900

    feat: add custom colors and utility plugins to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,30 @@
+/** @type {import('tailwindcss').Config} */
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        main: "#10b981",
+        kakao: "#f9e000",
+        naver: "#03c75a"
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".flexCol": { "@apply flex flex-col": {} },
+        ".flexColCenter": { "@apply flex flex-col items-center": {} },
+        ".flexRowCenter": { "@apply flex items-center": {} },
+        ".navMenu": { "@apply flex gap-1 items-center cursor-pointer": {} },
+        ".categoryItem": { "@apply p-2 hover:bg-main hover:bg-opacity-50": {} },
+      });
+    }),
+  ],
 };
 export default config;
