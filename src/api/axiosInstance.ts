2ce59a12353fd0d2678265b0df5b272b87e42d08
yheseul <kangyheseul@naver.com>
2025-03-11 21:37:16 +0900

    feat: 세션 지원을 위한 axiosInstance에 withCredentials 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
 
 export default axiosInstance;
